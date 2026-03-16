1895d01b8f01a9752c414b60b140fb9782821ffb
Safuvank <safuvanvkm@gmail.com>
2026-03-16 22:59:15 +0530

    updated new things

---
+++ filter.js
@@ -0,0 +1,38 @@
+const nums = [1, -3, 6, -3, 3, 6, 2, -6, 2];
+
+const result = nums.filter((nums) => nums >= 0);
+
+console.log(result);
+
+// const trick = [1,2,3,4,6,3,0].filter(x => true)
+
+// console.log(trick)
+
+const words = ["hi", "hello", "cat", "javascript"];
+
+const res = words.filter((word) =>
+  word
+    .split("")
+    .map((char) => char > 4)
+    .join(""),
+);
+console.log(res);
+
+const word = "school";
+
+console.log(word.length);
+
+const number = [5, 10, 15];
+
+const sum = number.reduce((a, b) => a + b, 0);
+
+console.log(sum);
+
+const count = ["a", "b", "a","d", "a"];
+
+const countsum = count.reduce((acc, curr) => {
+  acc[curr] = (acc[curr] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(countsum);
---
+++ some.js
@@ -0,0 +1,13 @@
+const nums = [1,3,5,24,53]
+
+const res = nums.some(num => num % 2 === 0)
+
+console.log(res)
+
+// find negative
+
+const neg = [1,3,5,24,53]
+
+const result = neg.some(num => num < 0)
+
+console.log(result)
