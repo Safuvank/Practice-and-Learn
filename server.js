9f434f6788281a0b12c40cf6f757704760894234
Safuvank <safuvanvkm@gmail.com>
2026-03-26 10:32:24 +0530

    node and express

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-// server create from scratch 
+// // server create from scratch
+
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url === "/"){
+//         res.end("Home page")
+//     } else if (req.url === "/contact"){
+//         res.end("Contact page")
+//     }
+
+//     else {
+//         res.end();
+//     }
+// })
+
+// server.listen(3000, ()=>{
+//     console.log("Server is running")
+// })
+
+const express = require("express");
+const app = express();
 
 
 
-const http = require('http')
-
-const server = http.createServer((req,res)=>{
-    if(req.url === "/"){
-        res.end("Home page")
-    } else if (req.url === "/contact"){
-        res.end("Contact page")
-    }
-    
-    else {
-        res.end();
-    }
+app.get("/",(req,res)=>{
+    // res.send("Hellow from server")
+    res.send("I am second")
 })
 
-server.listen(3000, ()=>{
-    console.log("Server is running")
+app.use((req, res, next) => {
+  console.log("Aplication middle");
+  console.log("hwoo server")
+//   next();
+});
+
+app.listen(3000,()=>{
+    console.log("server is running on 3000")
 })
