51539fe470e5884ffcdb2bbbaa73c20ee6f69975
Safuvank <safuvanvkm@gmail.com>
2026-04-10 17:33:38 +0530

    asd

--- oldFiles/index.js
+++ oldFiles/index.js
@@ -9,16 +9,5 @@ function createUser (name,age, place){
 const user1 = createUser("safuvan",12,"valakkulam")
 console.log(user1)
 
-function createCandidate (name, age){
-    return {
-        name,
-        age,
-        sayHello(){
-            console.log("Hello, " + name)
-        }
-    }
-}
 
-const cand = createCandidate("safuvan",10)
-console.log(cand.sayHello())
 
