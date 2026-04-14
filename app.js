a6e1fdf739252b83d84bc4a39445184163d75f08
Safuvank <safuvanvkm@gmail.com>
2026-04-14 11:08:43 +0530

    app

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "safuvan";
+let salary = 25000;
+// userName = 12
+function add(a, b) {
+    return a + b;
+}
+let result = add(10, 20);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
 let userName: string = "safuvan";
 let salary: number = 25000;
 
-userName = 12
+// userName = 12
+
+function add (a:number, b:number): number {
+    return a+ b
+}
+
+let result = add(10,20)
+console.log(result)
