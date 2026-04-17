0f0cbff5e960476644729ede75bd00f633e23a77
Safuvank <safuvanvkm@gmail.com>
2026-04-17 12:27:59 +0530

    tuples

---
+++ build/tuples.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let newArray = [
+    "safuvan",
+    14,
+    "safuvan",
+    45,
+    54,
+    45,
+];
+let newArray1 = ["ihsan"];
+//# sourceMappingURL=tuples.js.map
---
+++ tuples.ts
@@ -0,0 +1,10 @@
+let newArray: [string, number, string?, ...number[]] = [
+  "safuvan",
+  14,
+  "safuvan",
+  45,
+  54,
+  45,
+];
+
+let newArray1: [string, number?] = ["ihsan"];
