9f0e67c37d63de1ed00714e16ac0dbccf99e86b1
Safuvank <safuvanvkm@gmail.com>
2026-06-12 10:19:17 +0530

    create js folder and start revise js foundation

--- apiFetcing.jsx
+++ apiFetcing.jsx
@@ -1,21 +1,43 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-function App() {
-  const [users, setUsers] = useState([]);
+// function App() {
+//   const [users, setUsers] = useState([]);
 
-  const fetchUsers = async () => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users",
-    );
-    setUsers(response.data)
-  };
+//   const fetchUsers = async () => {
+//     const response = await axios.get(
+//       "https://jsonplaceholder.typicode.com/users",
+//     );
+//     setUsers(response.data)
+//   };
   
+//   useEffect(()=>{
+//     fetchUsers()
+//   },[])
+
+  
+//   return <div>App</div>;
+// }
+
+// export default App;
+
+
+import { setHeapSnapshotNearHeapLimit } from 'node:v8'
+import React from 'react'
+
+function apiFetcing() {
+  const [users,setUsers] = setState([])
+
+  const fetchUser = async() =>{
+    const response = await axios.get("lkj")
+    setUsers(resoponse.data)
+  }
+
   useEffect(()=>{
-    fetchUsers()
+    fetchUser()
   },[])
-
-  
-  return <div>App</div>;
+  return (
+    <div>apiFetcing</div>
+  )
 }
 
-export default App;
+export default apiFetcing
